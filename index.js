90f0c167f7b76fb8aec9d4dc04c4b115203dd60c
hongchunsim <chunsim015@gmail.com>
2021-08-09 13:53:05 +0900

    20210809 비밀 설정 정보 보호 관리

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//app.use(express.urlencoded())
+
+// application/json
+app.use(bodyParser.json());
+//app.use(express.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hongchunsim:qwer1234@boilerplate.in7cx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World !!'))
+app.get('/', (req, res) => res.send('boiler-plate 만들기~'))
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할 때 필요한 정보들을 클라이언트에서 가져오면 그것들을 데이터베이스에 넣어줌
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        //status(200) == '성공했다'
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
